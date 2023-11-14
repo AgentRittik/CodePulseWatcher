a22541329c49c7c06b44bbf0246d234c760f34ea
AgentRittik <109760373+AgentRittik@users.noreply.github.com>
2023-11-14 11:13:25 +0530

    completed the code to start listening to event when some comment on a PR

--- index.js
+++ index.js
@@ -2,13 +2,23 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
+
+// const commands = require('probot-commands');
 module.exports = (app) => {
   // Your code here
-  app.log.info("Yay, the app was loaded!");
+  app.log.info("Yay, the app was loaded! rittik");
 
-  app.on("issues.opened", async (context) => {
+  app.on(['issue_comment.created'], async (context) =>{
+    console.log("testing the comment event ");
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
+    });
+    return context.octokit.issues.createComment(issueComment);
+  });
+  app.on("issues.opened", async (context) => {
+    console.log(context.payload.issue.body);
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue! rittik",
     });
     return context.octokit.issues.createComment(issueComment);
   });
